91e6d0f643fea818168b82919489f2eca5d527a7
Hamzaabba2005 <hamza.abba@hotmail.com>
2026-04-09 17:08:34 +0100

    changes to config

--- src/config.py
+++ src/config.py
@@ -6,10 +6,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LABEL_COLS = ["N", "D", "G", "C", "A", "H", "M", "O"]
 
 IMG_SIZE = 224
-TRAIN_BS = 32
+TRAIN_BS = 16
 VAL_BS = 32
 EPOCHS = 100
-LR = 5e-5
-WEIGHT_DECAY = 1e-3
+LR = 3e-4
+WEIGHT_DECAY = 1e-4
 VAL_SIZE = 0.15
 SEED = 42
