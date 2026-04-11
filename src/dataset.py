a30d8a5f234441adb36464eb40c50fa06c451682
Hamzaabba2005 <hamza.abba@hotmail.com>
2026-04-11 20:36:55 +0100

    changes to dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -29,29 +29,35 @@ def create_dataloaders(
     val_samples,
     train_transform,
     val_transform,
+    test_samples=None,
     train_batch_size=64,
     val_batch_size=128,
     num_workers=4
 ):
     train_ds = ODIRDataset(train_samples, train_transform)
-    val_ds = ODIRDataset(val_samples, val_transform)
+    val_ds = ODIRDataset(val_samples,   val_transform)
 
     train_loader = DataLoader(
-        train_ds,
-        batch_size=train_batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        pin_memory=True,
+        train_ds, 
+        batch_size=train_batch_size, 
+        shuffle=True,  
+        num_workers=num_workers, 
+        pin_memory=True, 
         persistent_workers=True
     )
 
     val_loader = DataLoader(
-        val_ds,
-        batch_size=val_batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=True,
+        val_ds,   
+        batch_size=val_batch_size,   
+        shuffle=False, 
+        num_workers=num_workers, 
+        pin_memory=True, 
         persistent_workers=True
     )
+
+    if test_samples is not None:
+        test_ds = ODIRDataset(test_samples, val_transform)
+        test_loader = DataLoader(test_ds, batch_size=val_batch_size, shuffle=False, num_workers=num_workers, pin_memory=True, persistent_workers=True)
+        return train_ds, val_ds, test_ds, train_loader, val_loader, test_loader
 
     return train_ds, val_ds, train_loader, val_loader
