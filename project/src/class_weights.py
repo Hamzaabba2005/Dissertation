16c60b50c4727b978ff154584de20e1ee5c8d21c
Hamzaabba2005 <hamza.abba@hotmail.com>
2026-04-12 04:45:00 +0100

    file structure

--- project/src/class_weights.py
+++ project/src/class_weights.py
@@ -3,12 +3,6 @@ import torch
 
 
 def compute_pos_weight(samples):
-    """
-    Computes class weights for multilabel BCEWithLogitsLoss.
-
-    pos_weight = (#negative samples) / (#positive samples)
-    """
-
     Y = np.stack([s["label"] for s in samples], axis=0) 
 
     pos = Y.sum(axis=0)
